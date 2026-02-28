c89dea001e74a7e9365d98a5fbbd9780bf5efe43
aleynaaley <aleyna.erkulll@gmail.com>
2026-02-28 03:15:42 +0300

    update class2

--- basic_java/class1/class1.java
+++ basic_java/class1/class1.java
@@ -1,4 +1,4 @@
-public class aley{
+public class class1{
 
     public static void main(String[] args){
         System.out.println("Hello World");
--- basic_java/class2/calismaOrtami.java
+++ basic_java/class2/calismaOrtami.java
@@ -22,10 +22,31 @@ public class calismaOrtami {
 
         //float 3.40282347E+38 ve -3.40282347E+38 arası değer alabilir
         float e = 3.40282347E+38F; // F harfi float
-        System.out.println(e);      
+        float ee = Float.MAX_VALUE; // Float.MAX_VALUE ile en büyük float değerini alabiliriz
+        float eee = Float.MIN_VALUE; // Float.MIN_VALUE ile en küçük float değerini alabiliriz
+
+        System.out.println(e);
+        System.out.println(ee);
+        System.out.println(eee);
 
         //double 1.7976931348623157E+308 ve -1.7976931348623157E+308 arası değer alabilir
         double f = 1.7976931348623157E+308; // double
         System.out.println(f);  
+
+        boolean g = true; // boolean sadece true veya false değerlerini alabilir
+        boolean gg = 5 < 2;
+        System.out.println(g);
+        System.out.println(gg);
+
+        char h = 'A'; // char tek karakter tutar ve tek tırnak içinde yazılır
+        char hh = 65; // char ASCII değerlerini de tutabilir
+        System.out.println(h);
+        System.out.println(hh); 
+
+        String i = "Hello World"; // String birden fazla karakter tutar ve çift tırnak içinde yazılır
+        String ii = "Java Programming" + " is fun"; // String birleştirme işlemi yapılabilir
+        System.out.println(i);
+        System.out.println(ii); 
+
     }
 }
